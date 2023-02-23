f651d6f711db2fdf66c619e82f5f67e207ec8ea9
Carl97VM <barcacarlosv@gmail.com>
2023-02-22 22:02:28 -0400

    Desarrollo del proyecto Agenda

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <button class="btn btn-primary">Prymary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Success</button>
+  </div>
+</template>
+
+<script>
+
+</script>
