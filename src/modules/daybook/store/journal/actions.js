f748f96c6e61a292a23326ad2b9272516f4b2216
Carl97VM <barcacarlosv@gmail.com>
2023-02-28 23:28:05 -0400

    configuracion journal vuex

---
+++ src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,16 @@
+
+// export const myAction = async () => {
+
+// }
+
+export const loadEntries = async (/*{ commit }*/) => {
+
+}
+
+export const updataEntries = async (/*{ commit }*/) => {
+
+}
+
+export const createEntries = async (/*{ commit }*/) => {
+
+}
