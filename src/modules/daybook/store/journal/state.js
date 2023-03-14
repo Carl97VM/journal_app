a676fd5e3f663996a8c52ef2c0f16b4c9e9b14c7
Carl97VM <barcacarlosv@gmail.com>
2023-03-14 19:24:03 -0400

    Mutations, Estate, Actions, para mostrar datos FB

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,24 +1,25 @@
 
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: '1', // 12371298317892
-            date: new Date().toDateString(), // sat 23, julio
-            text: 'Historia 1',
-            picture: null, // https://
-        },
-        {
-            id: '2', // 12371298317892
-            date: new Date().toDateString(), // sat 23, julio
-            text: 'Historia 2',
-            picture: null, // https://
-        },
-        {
-            id: '3', // 12371298317892
-            date: new Date().toDateString(), // sat 23, julio
-            text: 'Historia 3',
-            picture: null, // https://
-        },
-    ]
+    // entries: [
+    //     {
+    //         id: '1', // 12371298317892
+    //         date: new Date().toDateString(), // sat 23, julio
+    //         text: 'Historia 1',
+    //         picture: null, // https://
+    //     },
+    //     {
+    //         id: '2', // 12371298317892
+    //         date: new Date().toDateString(), // sat 23, julio
+    //         text: 'Historia 2',
+    //         picture: null, // https://
+    //     },
+    //     {
+    //         id: '3', // 12371298317892
+    //         date: new Date().toDateString(), // sat 23, julio
+    //         text: 'Historia 3',
+    //         picture: null, // https://
+    //     },
+    // ]
+    entries: []
 })
